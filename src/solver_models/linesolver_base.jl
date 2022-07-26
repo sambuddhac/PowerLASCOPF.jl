75e5eb3e5862781bfd9bf442e1f7c01267aeb8f4
sambuddhac <sambuddha.chakrabarti@gmail.com>
2022-07-26 14:42:12 -0600

    Reformulated the line solver optimization model

--- src/solver_models/linesolver_base.jl
+++ src/solver_models/linesolver_base.jl
@@ -20,22 +20,22 @@ using Cbc
 using Ipopt
 using MathOptInterface
 
-function linesolverBase(
-    lambda_TXR::Array, # APP Lagrange Multiplier corresponding to the complementary slackness
-    ECoeff::Array, #Line temperature evolution coefficients
-    PgNextNu::Array, # Previous iterates of the corresponding decision variable values
+function linesolver_base(
+    lambda_txr::Array, # APP Lagrange Multiplier corresponding to the complementary slackness
+    ecoeff::Array, #Line temperature evolution coefficients
+    Pg_next_nu::Array, # Previous iterates of the corresponding decision variable values
     BSC::Array, # Cumulative disagreement between the generator output values for the previous and next intervals by the present, next, and the previous intervals, at the previous iteration
-    ETempCoeff::Array;
-    alphaFactor=0.05, #Fraction of line MW flow, which is the Ohmic loss
-    betaFactor=,
-    beta=, # APP tuning parameter for across the dispatch intervals
-    gamma=, # APP tuning parameter for across the dispatch intervals
-    PtMax=100000, # Line flow MW Limits
-    tempInit=340, #Initial line temperature in Kelvin
-    tempAmb=300, #Ambient temperature in Kelvin
-    maxTemp=473, #Maximum allowed line temerature in Kelvin
-    RNDInt=6, #Number of intervals for restoration to nominal/normal flows
-    ContCount=1, #Number of contingency scenarios
+    E_temp_coeff::Array;
+    alpha_factor=0.05, #Fraction of line MW flow, which is the Ohmic loss
+    beta_factor=0.02, ##Dummy Value
+    beta=0.5, # APP tuning parameter for across the dispatch intervals ##Dummy Value
+    gamma=0.25, # APP tuning parameter for across the dispatch intervals ##Dummy Value
+    Pt_max=100000, # Line flow MW Limits
+    temp_init=340, #Initial line temperature in Kelvin
+    temp_amb=300, #Ambient temperature in Kelvin
+    max_temp=473, #Maximum allowed line temerature in Kelvin
+    RND_int=6, #Number of intervals for restoration to nominal/normal flows
+    cont_count=1, #Number of contingency scenarios
     )
     start_t = now()
     if solChoice == 1
@@ -51,25 +51,25 @@ function linesolverBase(
     else
         error("Invalid Solver Choice:", solChoice)
     end
-    One = repeat([1], ContCount, (RNDInt-1))
+    One = repeat([1], cont_count, (RND_int-1))
 
     @variables model begin
-        0 <= Pt # Generator real power output
-        0 <= PtNext[1:ContCount, 1:(RNDInt-1)] # Generator's belief about its output in the next interval
+        0 <= Pt # Transmission Line real power flow
+        0 <= Pt_next[1:cont_count, 1:(RND_int-1)] # Transmission Line's belief about its flow in the next interval
     end
 
     @constraints model begin
-        PtNext .<= One * PtMax
-        PtNext .>= One * -PtMax
-        for contInd in 1:ContCount 
-            for omega in 1:RNDInt
-                ECoeff[omega]*tempInit+(1-ECoeff[omega])*tempAmb+(alphaFactor/betaFactor)*(sum((ETempCoeff[i, omega]*(PtNext[contInd, j])^2) for j in 1:(RNDInt-omega))) <= maxTemp
+        Pt_next .<= One * Pt_max
+        Pt_next .>= One * -Pt_max
+        for cont_ind in 1:cont_count 
+            for omega in 1:RND_int
+                ecoeff[omega]*temp_init+(1-ecoeff[omega])*temp_amb+(alpha_factor/beta_factor)*(sum((E_temp_coeff[cont_ind, omega]*(Pt_next[cont_ind, j])^2) for j in 1:(RND_int-omega))) <= max_temp
             end
         end
     end
 
-    @NLobjective(model, Min, (beta/2)*(sum(sum((PtNext[i, j]-PtNextNu[i, j])^2 for i in 1:ContCount) for j in 1:(RNDInt-1)))
-    +(gamma)*(sum(sum(PtNext[i, j]*BSC[i, j] for i in 1:ContCount) for j in 1:(RNDInt-1)))+sum(sum(PtNext[i, j]*lambda_TXR[i, j] for i in 1:ContCount) for j in 1:(RNDInt-1)))
+    @NLobjective(model, Min, (beta/2)*(sum(sum((Pt_next[i, j]-Pt_next_nu[i, j])^2 for i in 1:cont_count) for j in 1:(RND_int-1)))
+    +(gamma)*(sum(sum(Pt_next[i, j]*BSC[i, j] for i in 1:cont_count) for j in 1:(RND_int-1)))+sum(sum(Pt_next[i, j]*lambda_txr[i, j] for i in 1:cont_count) for j in 1:(RND_int-1)))
 
     optimize!(model)
     elapsed = now() - start_t
@@ -89,7 +89,7 @@ function linesolverBase(
 
     results = Dict(
         "Line_Self_Flow_Belief" => JuMP.value(Pt),
-        "Line_Next_Flow_Belief" => JuMP.value(PtNext),
+        "Line_Next_Flow_Belief" => JuMP.value(Pt_next),
         "Line_Optimal_Objective" => JuMP.objective_value(model),
     )
 
