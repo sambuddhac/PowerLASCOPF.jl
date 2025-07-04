13dba40b59391697f5fdcc3e2a0d315afba8d489
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2025-07-04 13:23:41 -0400

    Update ExtendedHydroGenerationCost.jl

--- src/models/solver_models/ExtendedHydroGenerationCost.jl
+++ src/models/solver_models/ExtendedHydroGenerationCost.jl
@@ -1,16 +1,16 @@
 """
 	@kwdef mutable struct ExtendedHydroGenerationCost{T<:GenIntervals}<:AbstractModel
-    		hydro_cost_core::HydroGenerationCost # Coefficient of the quadratic term
+    		hydro_cost_core::PSY.HydroGenerationCost # Coefficient of the quadratic term
     		regularization_term::T # Regularization Term
 	end
 	This is the struct for implmenting extended hydro generation cost model with additional regularization term. This is needed for solving (N-1-1)
 	contingency cases in the extended hydro generation cost model.
-        - hydro_cost_core::HydroGenerationCost # Coefficient of the quadratic term
+        - hydro_cost_core::PSY.HydroGenerationCost # Coefficient of the quadratic term
         - regularization_term::T # Regularization Term
 """
 
 @kwdef mutable struct ExtendedHydroGenerationCost{T<:GenIntervals}<:AbstractModel
-    hydro_cost_core::HydroGenerationCost # Coefficient of the quadratic term
+    hydro_cost_core::PSY.HydroGenerationCost # Coefficient of the quadratic term
     regularization_term::T # Regularization Term
 end
 
@@ -18,8 +18,12 @@ end
 get_variable(value::ExtendedHydroGenerationCost) = PSY.get_variable(value.hydro_cost_core)
 """Get [`ExtendedHydroGenerationCost`](@ref) `fixed`."""
 get_fixed(value::ExtendedHydroGenerationCost) = PSY.get_fixed(value.hydro_cost_core)
+"""Get [`ExtendedHydroGenerationCost`](@ref) `regularization_term`."""
+get_regularization_term(value::ExtendedHydroGenerationCost) = value.regularization_term
 
 """Set [`ExtendedHydroGenerationCost`](@ref) `variable`."""
 set_variable!(value::ExtendedHydroGenerationCost, val) = value.hydro_cost_core.variable = val
 """Set [`ExtendedHydroGenerationCost`](@ref) `fixed`."""
 set_fixed!(value::ExtendedHydroGenerationCost, val) = value.hydro_cost_core.fixed = val
+"""Set [`ExtendedHydroGenerationCost`](@ref) `regularization_term`."""
+set_regularization_term!(value::ExtendedHydroGenerationCost, val) = value.regularization_term = val
