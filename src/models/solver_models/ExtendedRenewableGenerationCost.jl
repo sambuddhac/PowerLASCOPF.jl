237a3988cca6a12a37e5cfabb591cf2801723215
mvghavidel <66364557+mvghavidel@users.noreply.github.com>
2025-07-11 12:27:43 +0100

    Update ExtendedRenewableGenerationCost.jl

--- src/models/solver_models/ExtendedRenewableGenerationCost.jl
+++ src/models/solver_models/ExtendedRenewableGenerationCost.jl
@@ -14,6 +14,12 @@
     regularization_term::T # Regularization Term
 end
 
+ExtendedRenewableGenerationCost(renewable_cost_core, regularization_term) = ExtendedRenewableGenerationCost(; renewable_cost_core, regularization_term)
+
+function ExtendedRenewableGenerationCost(::Nothing)
+    ExtendedRenewableGenerationCost(RenewableGenerationCost(nothing), 0.0)
+end
+
 """Get [`ExtendedRenewbleGenerationCost`](@ref) `variable`."""
 get_variable(value::ExtendedRenewableGenerationCost) = PSY.get_variable(value.renewable_cost_core)
 """Get [`ExtendedRenewbleGenerationCost`](@ref) `fixed`."""
@@ -25,10 +31,15 @@ get_shut_down(value::ExtendedRenewableGenerationCost) = PSY.get_shut_down(value.
 """Get [`ExtendedRenewableGenerationCost`](@ref) `regularization_term`."""
 get_regularization(value::ExtendedRenewableGenerationCost) = value.regularization_term
 """Get [`ExtendedRenewableGenerationCost`](@ref) `cost_core`."""
-get_cost_core(value::ExtendedRenewableGenerationCost) = value.renewable_cost_core
+get_cost_core(value::ExtendedRenewableGenerationCost) = PSY.value.renewable_cost_core
 """Get [`ExtendedRenewableGenerationCost`](@ref) `curtailment_cost`."""
+
+
 get_curtailment_cost(value::ExtendedRenewableGenerationCost) = PSY.get_curtailment_cost(value.renewable_cost_core)
 """Set [`ExtendedRenewableGenerationCost`](@ref) `variable`."""
 set_variable!(value::ExtendedRenewableGenerationCost, val) = value.renewable_cost_core.variable = val
 """Set [`ExtendedRenewableGenerationCost`](@ref) `curtailment_cost`."""
 set_curtailment_cost!(value::ExtendedRenewableGenerationCost, val) = value.renewable_core_cost.curtailment_cost = val
+set_regularization!(value::ExtendedRenewaableGenerationCost, val) = value.regularization_term = val
+"""Set [`ExtendedRenewableGenerationCost`](@ref) `cost_core`."""
+set_cost_core(value::ExtendedRenewableGenerationCost, cost_core) = value.renewable_cost_core = cost_core
